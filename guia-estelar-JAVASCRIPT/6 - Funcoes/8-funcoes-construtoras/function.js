f4f3f0a763fdc284827efcd1f7396b2a914aa623
awpadilha <padilha.a.w@gmail.com>
2021-03-22 18:54:20 -0300

    Guia Estelar JavaScript

---
+++ guia-estelar-JAVASCRIPT/6 - Funcoes/8-funcoes-construtoras/function.js
@@ -0,0 +1,20 @@
+/*
+    Function() constructor
+
+    * expressao new
+    * criar um novo objeto
+    * this keyword
+
+*/
+
+function Person(name) {
+    this.name = name
+    this.walk = function() {
+        return this.name + " está andando"
+    }
+}
+
+const mayk = new Person("Mayk")
+const joao = new Person("João")
+console.log(mayk.walk())
+console.log(joao.walk())
